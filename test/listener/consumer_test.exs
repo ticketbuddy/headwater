066569ffec8fa3cbbe4e539b6512d0b6e73544b4
James Bowers <james@james-bowers.me>
2020-03-17 19:37:12 +0000

    consumer unhappy path

--- test/listener/consumer_test.exs
+++ test/listener/consumer_test.exs
@@ -80,5 +80,50 @@ defmodule Headwater.Listener.ConsumerTest do
                  state
                )
     end
+
+    test "unhappy path", %{opts: opts, recorded_events: recorded_events} do
+      from_pid = self()
+      state = opts
+
+      FakeApp.PrinterMock
+      |> expect(:listener_prefix, 2, fn -> "a_listener_prefix_" end)
+
+      FakeApp.PrinterMock
+      |> expect(:handle_event, 2, fn
+        _expected_event = 1,
+        %{
+          aggregate_number: 4,
+          effect_idempotent_key: _idempotency_key,
+          event_id: "aaa-bbb-ccc-ddd-eee",
+          event_number: 4,
+          event_occurred_at: _event_occurred_at
+        } ->
+          :ok
+
+        _expected_event = 3745,
+        %{
+          aggregate_number: 5,
+          effect_idempotent_key: _idempotency_key,
+          event_id: "fff-ggg-hhh-iii-jjj",
+          event_number: 5,
+          event_occurred_at: _event_occurred_at
+        } ->
+          :error
+      end)
+
+      FakeApp.EventStoreMock
+      |> expect(:bus_has_completed_event_number, 1, fn
+        [bus_id: "a-consumer-test-bus-id", event_number: 4] -> :ok
+        [bus_id: "a-consumer-test-bus-id", event_number: 5] -> :ok
+      end)
+
+      assert_raise RuntimeError, fn ->
+        Headwater.Listener.Consumer.handle_events(
+          [recorded_events.one, recorded_events.two],
+          from_pid,
+          state
+        )
+      end
+    end
   end
 end
