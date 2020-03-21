3f84446e211cc0c22ac72b4811a5d8caa60de95a
James Bowers <james@james-bowers.me>
2020-03-21 23:05:46 +0000

    add todo reminder for adding idempotency key for event_handling in listeners

--- lib/headwater/listener/event_handler.ex
+++ lib/headwater/listener/event_handler.ex
@@ -61,6 +61,8 @@ defmodule Headwater.Listener.EventHandler do
 
     case result do
       {:submit, wish_or_wishes} ->
+
+        # TODO router.handle/1 should also take idempotency key.
         result = wish_or_wishes |> List.wrap() |> Enum.map(&router.handle/1)
 
         handler_result(result, handler, recorded_event)
